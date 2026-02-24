fa71cd979cf44258b60c10072c5b5a68019954f2
Dhiman Arya <dhiman.arya@cimpress.com>
2026-02-24 22:31:35 +0530

    Add seed script with sample tenants and variants

---
+++ src/seed.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+import { connectDB } from "./config/db";
+import { Tenant } from "./models/Tenant";
+import { Variant } from "./models/Variant";
+
+const seed = async () => {
+  await connectDB();
+
+  await mongoose.connection.dropDatabase();
+
+  const tenant1 = await Tenant.create({ name: "Tenant A" });
+  const tenant2 = await Tenant.create({ name: "Tenant B" });
+
+  await Variant.create([
+    {
+      tenantId: tenant1._id,
+      productId: new mongoose.Types.ObjectId(),
+      sku: "A-TSHIRT-M-RED",
+      attributes: { size: "M", color: "Red" },
+      price: 100,
+      stock: 10,
+      reorderLevel: 3,
+    },
+    {
+      tenantId: tenant2._id,
+      productId: new mongoose.Types.ObjectId(),
+      sku: "B-TSHIRT-L-BLUE",
+      attributes: { size: "L", color: "Blue" },
+      price: 120,
+      stock: 5,
+      reorderLevel: 2,
+    },
+  ]);
+
+  console.log("Seed completed");
+  process.exit(0);
+};
+
+seed();
